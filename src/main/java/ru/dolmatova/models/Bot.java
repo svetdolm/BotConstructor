a8939937aa9ea33c0d94f70bdb009598a8bfa50f
svetdolm <svetdolm@mail.ru>
2021-04-02 01:58:11 +0300

    1) Добавила классы Bot и Step; 2) Добавила переменные к классам;

---
+++ src/main/java/ru/dolmatova/models/Bot.java
@@ -0,0 +1,10 @@
+package ru.dolmatova.models;
+
+import java.util.List;
+
+public class Bot {
+    private int id;
+    private String name;
+    private String token;
+    private List<Step> steps;
+}
